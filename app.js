a79b2d0c663a14e6b386309a0e69635a13b26f2a
fangdongxia <fangdongxia@niwodai.net>
2017-11-16 13:24:25 +0800

    'eidt'

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jade = require('jade');
 
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-
 var app = express();
 var fs = require('fs');
 var multer = require('multer');
@@ -26,9 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', index);
-// app.use('/users', users);
 
 app.use(bodyParser.urlencoded({
 	// uploadDir: './uploads',
